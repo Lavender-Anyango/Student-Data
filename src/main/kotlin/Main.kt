f5598bb33711277ade86c61a5064706a1cb949c3
Lavender <anyangolavenderr@gmail.com>
2024-02-08 21:10:31 +0300

    variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    val name = "Lavender Anyango"
+    val age = 23
+    val phoneNumber = "0759972442"
+    val weight = 59.20
+    val isKenyan = true
+
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(isKenyan)
+
+}
